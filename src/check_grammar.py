9f0fe77976fb566e12674c75dc20dd20526d7c53
Igor Santarek <jegor377@gmail.com>
2025-03-03 01:33:09 +0100

    Count grammar error instead of putting label

--- src/check_grammar.py
+++ src/check_grammar.py
@@ -9,7 +9,7 @@ def add_grammar_check_column(dataset_name: str,
                              tool: LanguageTool) -> None:
     df = pd.read_csv(f"data/result/question/{dataset_name}.tsv", sep="\t")
 
-    grammar_labels = []
+    grammar_errors_count = []
     errors: Counter[str] = Counter()
 
     print(f"checking grammar for {dataset_name}...")
@@ -17,15 +17,14 @@ def add_grammar_check_column(dataset_name: str,
     for _, row in tqdm(df.iterrows(), total=df.shape[0]):
         gramatical_errors = tool.check(str(row["statement"]))
 
-        label = "correct" if len(gramatical_errors) != 0 else "incorrect"
-        grammar_labels.append(label)
+        grammar_errors_count.append(len(gramatical_errors))
         for match in gramatical_errors:
-            print(match.ruleId)
             errors[match.ruleId] += 1
 
-    df["grammar"] = grammar_labels
+    df["grammar_errors"] = grammar_errors_count
 
-    error_df = pd.DataFrame(errors)
+    error_df = pd.DataFrame.from_dict(errors, orient='index').reset_index()
+    error_df.columns = ["rule_id", "count"]
     error_df.to_csv(f"data/result/grammar/errors/{dataset_name}.tsv",
                     sep="\t", index=False)
 
